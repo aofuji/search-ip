c30fdf24b263df2f91ccad9a33bd585d6abf4a6b
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2023-11-26 15:52:00 -0300

    Criado metodo de busca

--- app/app.go
+++ app/app.go
@@ -1,11 +1,46 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicacao de busca"
 	app.Usage = "Busca IPs e nomes de servidro na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de enderecos na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "teste.com.br",
+				},
+			},
+			Action: buscaIps,
+		},
+	}
+
 	return app
 }
+
+func buscaIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+// go run main.go ip --host amazon.com.br
